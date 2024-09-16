fb0f153749c5c1fb35fd9378efb4ff0a0edefe05
aleksandre-svg <datodzukaevi38@gmail.com>
2024-09-16 20:59:54 +0400

    commit

---
+++ level 24/homework/hw2.py
@@ -0,0 +1,8 @@
+user = int(input("Print any number: "))
+user1 = int(input("Print any number: "))
+
+sum = user + user1
+sum1 = user * user1
+
+print("Plus: ", sum)
+print("Multiple", sum1)
---
+++ level 25/classwork/cw.py
@@ -0,0 +1,3 @@
+names = ["Vishreka", "JImshera", "LAvrenti", "Jemaliko"]
+numbers = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+numbers5 = [5, 10, 15, 20, 25, 30]
