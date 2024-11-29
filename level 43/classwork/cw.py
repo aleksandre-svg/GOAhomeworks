74acadfbfe1fab84cca5d1c906579f2af17a2caf
Diana044 <dzukaevadiana3@gmail.com>
2024-11-29 16:22:08 +0400

    commit

--- level 43/classwork/cw.py
+++ level 43/classwork/cw.py
@@ -1,1 +1,12 @@
-#codewars day
+list=[1,2,3,4,5]
+def positive_sum(arr):
+    sum = 0
+    if len(arr) == 0:
+        return 0
+    else:
+        for i in arr:
+            sum += arr
+        return sum
+print(positive_sum([1,2,3,4,5]))
+        
+        
