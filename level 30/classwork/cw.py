44381a457fa2c5f75d9449190a19ac3e20ae32b7
Diana044 <dzukaevadiana3@gmail.com>
2024-12-16 20:06:58 +0400

    commit

---
+++ level 30/classwork/cw.py
@@ -0,0 +1,12 @@
+mentors = []
+avarage = []
+mentors1 = ['gabrieli', 'sandro', 'daviti', 'vajo']
+name = input("Enter your name: ")
+if name == mentors1[0] or name == mentors1[1] or name == [mentors1[2] or name == mentors1[3]]:
+    mentors.append(name)
+else:
+    avarage.append(name)
+print('mentors: ')
+print(mentors)
+print('avrage names: ')
+print(avarage)
