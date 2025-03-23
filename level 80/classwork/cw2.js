6e5d71b6d497082b29874c0ed8b159b7e56555f6
Diana044 <dzukaevadiana3@gmail.com>
2025-03-23 21:12:54 +0400

    commit

---
+++ level 80/classwork/cw2.js
@@ -0,0 +1,10 @@
+let img = document.getElementById("img")
+
+
+img.addEventListener('mouseover', function(){
+    img.src = 'cw2.1.png'
+})
+
+img.addEventListener('mouseout', function(){
+    img.src = "cw2.png"
+})
